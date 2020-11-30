7e3ef85b7a93e67214275d68b20c630022ad429e
Shaikh Abdul Sami <65606480+ShaikhAbdulSami@users.noreply.github.com>
2020-11-30 14:16:47 +0500

    updated

--- src/components/dynamicLollyPage.tsx
+++ src/components/dynamicLollyPage.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import Lolly from "./lolly"
 import { graphql , Link } from "gatsby"
 import Header from "./header"
-import { useQuery } from "@apollo/client/react/hooks/useQuery"
 
 export const query = graphql`
   query($lollyPath: String!) {
@@ -19,37 +18,6 @@ export const query = graphql`
     }
   }
 `
-
-// export default function DynamicLollyPage({ data }) {
-
-//   return (
-//     <div>
-//       <Header
-        // mainHeadingText="Virtual Lolly"
-        // secondaryHeadingText="You recieved a lolly, dont eat it alone !"
-//       />
-//       <h5 className="sharableLinkContainer">Your shareable link: </h5>{" "}
-//       <span className="sharableLink">
-//         {" "}
-//         {`https://shaikh-virtual-lolly.netlify.app/lollies/${data?.LOLLIES?.getLollyByPath?.lollyPath}`}
-//       </span>
-//       <div className="recievedContentContainer">
-        // <Lolly
-        //   style="lollyRecieved"
-        //   lollyTop={data?.LOLLIES?.getLollyByPath?.flavorTop}
-        //   lollyMid={data?.LOLLIES?.getLollyByPath?.flavorMid}
-        //   lollyBot={data?.LOLLIES?.getLollyByPath?.flavorBot}
-        // />
-
-//         <div className="recievedTextContainer">
-//           <h3>HI {data?.LOLLIES?.getLollyByPath?.recipientName?.toUpperCase()}</h3>
-//           <p>{data?.LOLLIES?.getLollyByPath?.message}</p>
-//           <h4>From: {data?.LOLLIES?.getLollyByPath?.sendersName}</h4>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
 
 export default function LollyPage({ data }) {
 
@@ -68,7 +36,7 @@ export default function LollyPage({ data }) {
           <div className="freezedLollyData">
             <div className="linkWrapper">
               <h4>Share this link with your frined</h4>
-              <p>{`/lollies/${data?.LOLLIES?.getLollyByPath?.lollyPath}`}</p>
+              <p>{`https://shaikh-virtual-lolly.netlify.app/lollies/${data?.LOLLIES?.getLollyByPath?.lollyPath}`}</p>
             </div>
             <div className="freezedLollyCard">
               <h1>to: {data?.LOLLIES?.getLollyByPath?.recipientName}</h1>
