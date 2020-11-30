f7f69cc6d4a7dd6d53efe46d255ea4a7ec1e8a49
Shaikh Abdul Sami <65606480+ShaikhAbdulSami@users.noreply.github.com>
2020-11-30 11:30:46 +0500

    changed

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,29 @@
 
 
-// const path = require(`path`)
+const path = require(`path`)
 
-// exports.createPages = async ({ actions, graphql }) => {
-//   const { data } = await graphql(`
-//   query MyQuery {
-//     LOLLIES {
-//       getAllLollies {
-//         lollyPath
-//       }
-//     }
-//   }
-//   `)
+exports.createPages = async ({ actions, graphql }) => {
+  const { data } = await graphql(`
+  query MyQuery {
+    LOLLIES {
+      getAllLollies {
+        lollyPath
+      }
+    }
+  }
+  `)
 
-//   console.log(data)
-//   data.LOLLIES.getAllLollies.forEach(({ lollyPath }) => {
-//     actions.createPage({
-//       path: `/lollies/${lollyPath}`,
-//       component: path.resolve(`./src/components/dynamicLollyPage.tsx`),
-//       context: {
-//         lollyPath: lollyPath,
-//       },
-//     })
-//   })
-// }
+  console.log(data)
+  data.LOLLIES.getAllLollies.forEach(({ lollyPath }) => {
+    actions.createPage({
+      path: `/lollies/${lollyPath}`,
+      component: path.resolve(`./src/components/dynamicLollyPage.tsx`),
+      context: {
+        lollyPath: lollyPath,
+      },
+    })
+  })
+}
 
 
 
--- src/components/dynamicLollyPage.tsx
+++ src/components/dynamicLollyPage.tsx
@@ -1,51 +1,51 @@
-// import React from "react"
-// import Lolly from "./lolly"
-// import { graphql } from "gatsby"
-// import Header from "./header"
+import React from "react"
+import Lolly from "./lolly"
+import { graphql } from "gatsby"
+import Header from "./header"
 
-// export const query = graphql`
-//   query($lollyPath: String!) {
-//     LOLLIES {
-//       getLollyByPath(lollyPath: $lollyPath) {
-//         flavorBot
-//         flavorMid
-//         flavorTop
-//         lollyPath
-//         message
-//         recipientName
-//         sendersName
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query($lollyPath: String!) {
+    LOLLIES {
+      getLollyByPath(lollyPath: $lollyPath) {
+        flavorBot
+        flavorMid
+        flavorTop
+        lollyPath
+        message
+        recipientName
+        sendersName
+      }
+    }
+  }
+`
 
-// export default function DynamicLollyPage({ data }) {
+export default function DynamicLollyPage({ data }) {
 
-//   return (
-//     <div>
-//       <Header
-//         mainHeadingText="Virtual Lolly"
-//         secondaryHeadingText="You recieved a lolly, dont eat it alone !"
-//       />
-//       <h5 className="sharableLinkContainer">Your shareable link: </h5>{" "}
-//       <span className="sharableLink">
-//         {" "}
-//         {`https://localhost:8888/lollies/${data?.LOLLIES?.getLollyByPath?.lollyPath}`}
-//       </span>
-//       <div className="recievedContentContainer">
-//         <Lolly
-//           style="lollyRecieved"
-//           lollyTop={data?.LOLLIES?.getLollyByPath?.flavorTop}
-//           lollyMid={data?.LOLLIES?.getLollyByPath?.flavorMid}
-//           lollyBot={data?.LOLLIES?.getLollyByPath?.flavorBot}
-//         />
+  return (
+    <div>
+      <Header
+        mainHeadingText="Virtual Lolly"
+        secondaryHeadingText="You recieved a lolly, dont eat it alone !"
+      />
+      <h5 className="sharableLinkContainer">Your shareable link: </h5>{" "}
+      <span className="sharableLink">
+        {" "}
+        {`https://shaikh-virtual-lolly.netlify.app/lollies/${data?.LOLLIES?.getLollyByPath?.lollyPath}`}
+      </span>
+      <div className="recievedContentContainer">
+        <Lolly
+          style="lollyRecieved"
+          lollyTop={data?.LOLLIES?.getLollyByPath?.flavorTop}
+          lollyMid={data?.LOLLIES?.getLollyByPath?.flavorMid}
+          lollyBot={data?.LOLLIES?.getLollyByPath?.flavorBot}
+        />
 
-//         <div className="recievedTextContainer">
-//           <h3>HI {data?.LOLLIES?.getLollyByPath?.recipientName?.toUpperCase()}</h3>
-//           <p>{data?.LOLLIES?.getLollyByPath?.message}</p>
-//           <h4>From: {data?.LOLLIES?.getLollyByPath?.sendersName}</h4>
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
+        <div className="recievedTextContainer">
+          <h3>HI {data?.LOLLIES?.getLollyByPath?.recipientName?.toUpperCase()}</h3>
+          <p>{data?.LOLLIES?.getLollyByPath?.message}</p>
+          <h4>From: {data?.LOLLIES?.getLollyByPath?.sendersName}</h4>
+        </div>
+      </div>
+    </div>
+  )
+}
